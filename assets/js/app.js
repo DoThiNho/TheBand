bb9e36ccc9e3558f37bcf19338c306a202c8d198
Do Thi Nho <dothinho2454@outlook.com>
2022-10-05 21:16:30 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// var modal = document.querySelector('.modal')
+// var btnBuyTicket = document.querySelectorAll('.place-buy-btn')
+// var modalClose = document.querySelector('.modal-close')
+
+// console.log(btnBuyTicket);
+
+// btnBuyTicket.forEach(function(element) {
+//     console.log(element);
+//     element.classList.add('open')
+// })
+
+// modalClose.onclick = function() {
+//     modal.classList.remove('open');
+// }
